fa22b6b3983a9f91e12ce9f40a022499d6bda765
tksuns <tksuns@gmail.com>
2020-05-27 12:44:36 +0900

    백준 10953 문제 풂

---
+++ bakjun/10953.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, a, b;
+    scanf("%d", &num);
+
+    while (num--)
+    {
+        scanf("%d,%d", &a, &b);
+        printf("%d\n", a + b);
+    }
+}
