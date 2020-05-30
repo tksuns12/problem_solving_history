719d68ce024b21a148260432ee19159d7a6480c7
tksuns12 <tksuns@gmail.com>
2020-05-30 10:05:35 +0900

    백준 2609 문제 풂

---
+++ bakjun/2609.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+    {
+        return a;
+    }
+    else
+    {
+        return gcd(b, a % b);
+    }
+}
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    int g = gcd(a, b);
+    printf("%d\n", g);
+    printf("%d", a * b / g);
+}
